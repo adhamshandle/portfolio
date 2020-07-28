4f321a5a7025e0b5f79339774796b8d91a115209
adhamImperial <adhamkhaled2427@gmail.com>
2020-07-29 01:51:15 +0200

    about processing

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,43 +1,60 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components'
-import {Card } from 'react-bootstrap'
+import { Card, Row, Col } from 'react-bootstrap'
 // import { postObject, getAllObjects } from "../../services/CommonServices";
 import { withRouter, Link } from 'react-router-dom'
-import '../../../node_modules/font-awesome/css/font-awesome.min.css'; 
+import { faLaptopCode, faMobileAlt,faTabletAlt,faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 
 const Container = styled.div`
 height: 100vh;
-width: 100%;
-display:flex;
-justify-content: center;
+text-align: center;
+margin-left: 10%;
+margin-right: 10%;
 margin-top:20px;
 `
 
 const Header = styled.h1`
 color: #1f2737;
 text-decoration: underline;
+width: 100%;
 
 `
 
 function About() {
   return (
     <Container >
-        <Header>
-            ABOUT
+      <Row>
+      <Header>
+        ABOUT
         </Header>
-        <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-      <i className="fas fa-laptop-code"></i> 
-    </Card.Text>
-  </Card.Body>
-</Card>
+        <Col md={3}>
+          <Card style={{ width: '15rem', height:'10rem', backgroundColor : '#1f2737' }}>
+            <Card.Body>
 
-  {/* <i class="fas fa-laptop-code"></i> */}
+              <FontAwesomeIcon icon={faLaptopCode} size="6x" color="#ed6c82" />
+              <Card.Text style={{color:'white', fontWeight:'700',position:'absolute',bottom:'5px',width:'80%'}}>Javascript Native</Card.Text>
+            </Card.Body>
+          </Card>
+
+        </Col>
+        <Col md={3}>
+          <Card style={{ width: '15rem', height:'10rem', backgroundColor : '#1f2737' }}>
+            <Card.Body>
+
+              <FontAwesomeIcon icon={faMobileAlt} size="3x" style={{padding:'5px'}} color="#ed6c82" />
+              <FontAwesomeIcon icon={faTabletAlt} size="4x" style={{padding:'5px'}} color="#ed6c82" />
+              <FontAwesomeIcon icon={faDesktop} size="5x" style={{padding:'5px'}} color="#ed6c82" />
+              <Card.Text style={{color:'white', fontWeight:'700',position:'absolute',bottom:'5px',width:'80%'}}>Responsive Design</Card.Text>
+            </Card.Body>
+          </Card>
+
+        </Col>
+      </Row>
+
+
+      {/* <i class="fas fa-laptop-code"></i> */}
     </Container>
   );
 }
